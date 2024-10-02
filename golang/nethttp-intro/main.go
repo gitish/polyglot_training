8b5bcf61532c79cb4a6ccd505afe1d4c12330e1a
ZEESHAN ALAM KHAN <zeeshan.maths@gmail.com>
2024-10-02 17:40:48 +0530

    polyglot training code for Oct 3rd, 2024 (#2)
    * json in go
    
    * net/http code
    
    * gin crud example

---
+++ golang/nethttp-intro/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// handler function to process incoming HTTP requests
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Headers: %s\n", r.Header)
+	fmt.Printf("Method: %s\n", r.Method)
+	fmt.Printf("Path: %s\n", r.URL.Path)
+	// Write "Hello, World!" to the response writer
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	// Register the handler function for the root URL ("/")
+	http.HandleFunc("/", handler)
+
+	// Start the HTTP server on port 8080 and listen for incoming requests
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		// If there's an error starting the server, log it and exit
+		fmt.Printf("Error starting server: %v\n", err)
+	}
+}
